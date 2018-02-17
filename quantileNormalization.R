3bf3d276b25718fe9d1839683c2f1390a93af09f
Rucheng Diao <diaorch@umich.edu>
2018-02-17 18:57:26 -0500

    added quantile normalization scripts dealing with ties

---
+++ quantileNormalization.R
@@ -0,0 +1,84 @@
+normalizeByQuantiles <- function(M, ties = TRUE){
+  # not roboust for NAs?
+  sampleNames <- colnames(M)
+  M <- M[complete.cases(M), ]
+  # M is a numeric matrix
+  if (is.vector(M)){
+    M <- as.matrix(M, ncol = 1)
+    colnames(M) <- sampleNames
+  }
+  
+  rankedM <- apply(X = M, MARGIN = 2, FUN = rank, ties.method = 'min')
+  print('ranked')
+  sortedM <- apply(X = M, MARGIN = 2, FUN = sort)
+  print('sorted')
+  if (!ties){
+    meanRowM <- apply(X = sortedM, MARGIN = 1, FUN = mean)
+    # function to retrive individual means from index
+    convertIndexToMean <- function(index, meanVector)(
+      return(meanVector[index])
+    )
+    mappedMeanM <- apply(X = rankedM, MARGIN = 2, convertIndexToMean, meanVector = meanRowM)
+  } else{
+    calcMeanWithTies <- function(indivRank, rankVec, sortedMtx){
+      rankIdx <- sum(rankVec == indivRank)
+      rankStart <- indivRank
+      rankEnd <- rankStart + rankIdx - 1
+      allValue <- as.vector(sortedMtx[rankStart:rankEnd, ])
+      return(mean(allValue))
+    }
+    mapMeanToVector <- function(rankVec, sortedMtx){
+      print(head(rankVec))
+      meanWithTiesVector <- sapply(X = rankVec, FUN = calcMeanWithTies, 
+                                   rankVec = rankVec, sortedMtx = sortedMtx)
+      return(meanWithTiesVector)
+    }
+    mappedMeanM <- apply(X = rankedM, MARGIN = 2, FUN = mapMeanToVector, 
+                         sortedMtx = sortedM)
+    print('mapped')
+  }
+  return(mappedMeanM)
+}
+
+### take args from command line
+args <- commandArgs(TRUE)
+
+## rarse arguments (we expect the form --arg=value)
+parseArgs <- function(x) strsplit(sub("^--", "", x), "=")
+argsDF <- as.data.frame(do.call("rbind", parseArgs(args)))
+
+## arguments
+inputFilename <- as.character(argsDF$V2[argsDF$V1 == 'input'])
+outputFilename <- as.character(argsDF$V2[argsDF$V1 == 'output'])
+startColumn <- as.character(argsDF$V2[argsDF$V1 == 'start'])
+startColumn <- as.numeric(startColumn)
+
+## read file
+inputDf <- read.csv(file = inputFilename, header = FALSE)
+
+print(head(inputDf))
+startColumn
+
+## subset matrix
+supplColumns <- inputDf[, 1:(startColumn - 1)]
+normColumns <- inputDf[, startColumn:ncol(inputDf)]
+
+print("subsetted")
+print(head(normColumns))
+
+## quantile normalize
+qNormedColumns <- normalizeByQuantiles(M = normColumns, ties = TRUE)
+
+print("normalized")
+
+## bind supplementary information and normalized matrix
+if (!is.null(supplColumns)){
+  outputDf <- as.data.frame(cbind(supplColumns, qNormedColumns))
+} else{
+  outputDf <- as.data.frame(qNormedColumns)
+}
+
+# print csv to screen
+print(head(outputDf))
+write.table(outputDf, file = outputFilename, sep = ',', row.names = FALSE, col.names = FALSE, quote = FALSE)
+
---
+++ quantileNormalization.py
@@ -0,0 +1,39 @@
+#!/use/bin/env python2
+"""
+quantile normalize a numeric data frame
+with ties treated as pooled average of all values corresponding quantiles
+
+Special thanks to StackOverflow post: 
+https://stackoverflow.com/questions/37935920/quantile-normalization-on-pandas-dataframe
+https://stackoverflow.com/questions/43280322/sort-all-columns-of-a-pandas-dataframe-independently-using-sort-values
+"""
+import numpy as np
+import pandas as pd
+
+def mapTieRankToMean(rankSeries, meanSeries):
+    mappedMean = rankSeries.apply(lambda x: np.mean(meanSeries[(x - 1):(x - 1 + sum(rankSeries == x))]))
+    # in anonymous use (x - 1) to adjust for indexing
+    return(mappedMean)
+
+def quantileNormalization(inputDf):
+    # remove NAs 
+    completeDf = inputDf.dropna(axis = 0, how = 'any')
+    # rank
+    rankDf = completeDf.rank(axis = 0, method = 'min', ascending = True)
+    rankDf = rankDf.astype(int) # risky when using some other ranking methods
+    # sort
+    sortDf = completeDf.copy()
+    sortDf = pd.DataFrame(np.sort(sortDf.values, axis = 0), index = sortDf.index, columns = sortDf.columns)
+    # calculate mean for assuming no tie ranks
+    rowMean = sortDf.mean(axis = 1, skipna = True)
+    # map mean values to rank, dealing with ties
+    mappedDf = rankDf.apply(lambda x: mapTieRankToMean(x, meanSeries = rowMean))
+    
+    return(mappedDf)
+    
+
+df = pd.DataFrame({'C1': {'A': 1, 'B': 2, 'C': 3, 'D': 4},
+                   'C2': {'A': 2, 'B': 4, 'C': 4, 'D': 6}})
+outputDf = quantileNormalization(df)
+
+print(outputDf)
